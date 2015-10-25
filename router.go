6c95aa941b369de64c68532792500a17262e6932
Vinicius Correa <viniciuscorrea91@gmail.com>
2015-10-25 17:58:38 -0200

    Trailing whitespaces

--- router.go
+++ router.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"strings"
 	"net/http"
+	"strings"
 
-  "github.com/asvins/router"
-	"github.com/unrolled/render"
 	"github.com/asvins/common_interceptors/logger"
+	"github.com/asvins/router"
+	"github.com/unrolled/render"
 )
 
 func DiscoveryHandler(w http.ResponseWriter, req *http.Request) {
-  prefix := strings.Join([]string{ServerConfig.Server.Addr, ServerConfig.Server.Port}, ":")
-  r := render.New()
+	prefix := strings.Join([]string{ServerConfig.Server.Addr, ServerConfig.Server.Port}, ":")
+	r := render.New()
 
 	//add discovery links here
-  discoveryMap := map[string]string {"discovery": prefix+"/api/discovery"}
+	discoveryMap := map[string]string{"discovery": prefix + "/api/discovery"}
 
 	r.JSON(w, http.StatusOK, discoveryMap)
 }
 
-func DefRoutes() *router.Router {  
-  r := router.NewRouter()
+func DefRoutes() *router.Router {
+	r := router.NewRouter()
 
 	r.AddRoute("/api/discovery", router.GET, DiscoveryHandler)
 
